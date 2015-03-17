95407648e19c3d11d4de39b15cf18d9ae278ff06
Andreas Lundahl <mail@andreaslundahl.com>
2015-03-17 21:07:59 +0100

    Start implementing GitHub integration

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
+import httplib
 
 # Create your views here.
+
+def github_hook(request):
+  conn = httplib.HTTPSConnection('api.github.com')
+  conn.request('GET', '/orgs/videumcodeup/repos', {}, {"User-Agent": "videumcodeup"})
+  repos = json.loads(conn.getresponse().read())
+  # for i in repos:
+  #   print(i['']
+
+  data = {"status": "ok"}
+  return HttpResponse(json.dumps(repos), content_type='application/json')
--- settings/urls.py
+++ settings/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from gallery.views import github_hook
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'settings.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns(
+  '',
+  url(r'^admin/', include(admin.site.urls)),
+  url(r'^github-hook', github_hook)
 )
