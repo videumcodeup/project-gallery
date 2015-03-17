6d900d33f0e2ef1d92323cdfd7bb8f7e747c37a1
John Herrlin <jherrlin@gmail.com>
2015-03-17 23:16:07 +0100

    database model Repo created, just made for testing. test user added to database. username:test, password:test, backend reached at: 127.0.0.1:8000/admin. Repo object added to admin.py. View updated to not duplicate entries

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# Test ORM object
+class Repo(models.Model):
+    github_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    svn_url = models.CharField(max_length=254)
+    watchers = models.IntegerField()
+    language = models.CharField(max_length=254, blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
--- gallery/views.py
+++ gallery/views.py
@@ -3,14 +3,32 @@ from django.http import HttpResponse
 import json
 import httplib
 
+from .models import Repo
 # Create your views here.
 
 def github_hook(request):
   conn = httplib.HTTPSConnection('api.github.com')
   conn.request('GET', '/orgs/videumcodeup/repos', {}, {"User-Agent": "videumcodeup"})
-  repos = json.loads(conn.getresponse().read())
-  # for i in repos:
-  #   print(i['']
+  repos_from_github = json.loads(conn.getresponse().read())
+
+  repos_in_database = Repo.objects.all()
+
+  repos_in_database_id = []
+  for i in repos_in_database:
+      repos_in_database_id.append(i.github_id)
+
+  for i in repos_from_github:
+
+      if i['id'] not in repos_in_database_id:
+          new_repo = Repo(
+              github_id = i['id'],
+              name = i['name'],
+              description = i['description'],
+              svn_url = i['svn_url'],
+              watchers = i['watchers'],
+              language = i['language']
+          )
+          new_repo.save()
 
   data = {"status": "ok"}
-  return HttpResponse(json.dumps(repos), content_type='application/json')
+  return HttpResponse(json.dumps(repos_from_github), content_type='application/json')
